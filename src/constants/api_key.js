de3bed0e23e3041e2bbe9b5870309f6a82f352d1
rbieti <rjbieti8@gmail.com>
2017-12-23 18:54:07 -0800

    Create api_key.js

---
+++ src/constants/api_key.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////
+// Keys to export (or use internally)
+
+// https://developers.google.com/places/web-service/
+export const GOOGLE_PLACES_API_KEY = "YOUR_KEY_HERE";
+
+// https://developers.facebook.com/apps/
+export const FACEBOOK_API_KEY = "YOUR_KEY_HERE";
+
+// https://console.firebase.google.com/
+export const GOOGLE_FIREBASE_CONFIG = {
+  apiKey: "YOUR_KEY_HERE",
+  authDomain: "YOUR_KEY_HERE",
+  databaseURL: "YOUR_KEY_HERE",
+  projectId: "YOUR_KEY_HERE",
+  storageBucket: "YOUR_KEY_HERE",
+  messagingSenderId: "YOUR_KEY_HERE"
+};
