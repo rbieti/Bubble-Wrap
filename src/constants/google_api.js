128110e8bb22ee847d7ffbcd69019dc972b6fa22
rbieti <rjbieti8@gmail.com>
2017-12-23 18:54:46 -0800

    Add files via upload

---
+++ src/constants/google_api.js
@@ -0,0 +1,4 @@
+export const GOOGLE_PLACES_ROOT_URL =
+  'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+export const GOOGLE_GEOCODE_ROOT_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
+export const GOOGLE_PLACES_PHOTO_ROOT_URL = 'https://maps.googleapis.com/maps/api/place/photo?';
---
+++ src/constants/style.js
@@ -0,0 +1,3 @@
+export const PRIMARY_COLOR = '#3d434c';
+export const SECONDARY_COLOR = '#4c493c';
+export const SECONDARY_COLOR_LIGHT = '#e2dec9';
