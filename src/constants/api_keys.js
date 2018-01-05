9b5666826021eb60b8a35965b7d40ab519c5f5e4
rbieti <rjbieti8@gmail.com>
2018-01-05 12:12:18 -0800

    change color api-keys and name

---
+++ src/constants/api_keys.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////
+// Keys to export (or use internally)
+
+// https://developers.google.com/places/web-service/
+//export const GOOGLE_PLACES_API_KEY = 'YOUR_KEY_HERE';
+
+// https://developers.facebook.com/apps/
+//export const FACEBOOK_API_KEY = 'YOUR_KEY_HERE';
+
+// https://console.firebase.google.com/
+export const GOOGLE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC7vzBCU5fbN9DGd4HF1RMrHwPUC4OpVx4",
+    authDomain: "bubble-wrap-8485d.firebaseapp.com",
+    databaseURL: "https://bubble-wrap-8485d.firebaseio.com",
+    projectId: "bubble-wrap-8485d",
+    storageBucket: "bubble-wrap-8485d.appspot.com",
+    messagingSenderId: "149081643013"
+};
