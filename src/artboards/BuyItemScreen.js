39be1e0093bb936b7d15af7ec9e14115d651d86d
Pake <13pake@gmail.com>
2018-01-24 11:48:32 -0800

    Create a separate screen scaffolding with BuilderX (also initial BuilderX commit so update .gitignore)

---
+++ src/artboards/BuyItemScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+export default class EditItemScreen extends Component {
+  render() {
+    return <View style={styles.root} />;
+  }
+}
+const styles = StyleSheet.create({
+  root: { backgroundColor: "white", flex: 1 }
+});
