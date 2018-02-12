3c467e9fcb293b7d8b2d2ad37b08c42263cb3467
Kyle Nakamura <knakamura13@apu.edu>
2018-02-12 12:17:14 -0800

    Implement basic mapview with react native maps

--- src/constants/google_api.js
+++ src/constants/google_api.js
@@ -2,3 +2,4 @@ export const GOOGLE_PLACES_ROOT_URL =
   'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
 export const GOOGLE_GEOCODE_ROOT_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
 export const GOOGLE_PLACES_PHOTO_ROOT_URL = 'https://maps.googleapis.com/maps/api/place/photo?';
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyACKiQZjA1_5v3sHd_MZQtKg0t1tJ3yuNc';
