6458b6ba62529937de5d368e2cc308af14dcebcb
Aakiyama12 <aakiyama12@apu.edu>
2018-03-12 12:23:41 -0700

    made it so items can be added to database as a standalone thing

--- src/actions/user_items_actions.js
+++ src/actions/user_items_actions.js
@@ -11,18 +11,18 @@ export const itemUpdate = ({ prop, value }) => {
   };
 };
 
-export const itemCreate = ({ name, description, price }) => {
+export const itemCreate = ({ name, description, price}) => {
   // console.log(name, description, price); // comment this
   const { currentUser } = firebase.auth();
 
-  firebase.database().ref(`/users/${currentUser.uid}/items`)
-    .push({ name, description, price });
+  firebase.database().ref(`/items`)
+    .push({ name, description, price});
 };
 
 export const fetchItems = () => {
   return dispatch => {
     const { currentUser } = firebase.auth();
-    const recentPostsRef = firebase.database().ref(`/users/${currentUser.uid}/items`);
+    const recentPostsRef = firebase.database().ref(`/users/${currentUser.uid}/items`); //this needs to be edited so it pulls items correctly - the updated  way we're storing items
     
     //recentPostsRef.once('value').then(snapshot => {
     recentPostsRef.on('value', snapshot => {
