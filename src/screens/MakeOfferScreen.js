1d38c2328594f65845b679c128694c2d1bd1d94b
Kyle Nakamura <knakamura13@apu.edu>
2018-03-23 10:18:15 -0700

    Finish makeofferscreen redesign and add item001 image

--- src/screens/MakeOfferScreen.js
+++ src/screens/MakeOfferScreen.js
@@ -1,101 +1,95 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Button,TouchableOpacity, Image } from "react-native";
+import { View, StyleSheet, Text, Button, TouchableOpacity, Image, TextInput } from "react-native";
 import { PRIMARY_COLOR } from '../constants/style';
 export default class Untitled extends Component {
-    static navigationOptions = ({ navigation }) => ({
-        //tabBarVisible: false,
-        title: 'MakeOfferScreen',
-        tabBarLabel: 'MakeOfferScreen',
-        headerTitleStyle: {
-          textAlign: 'center',
-          alignSelf: 'center'
-        },
-        /*
-        headerLeft: (
-        
-          <Button
-          //  navigate={navigation.navigate}
-            large
-            icon={{ name: 'menu' }}
-            backgroundColor={PRIMARY_COLOR}
-            onPress={() => navigation.navigate('DrawerOpen')}
-          />
-         
-        ),
-        drawerIcon: ({ tintColor }) => (
-          <Icon name="home" size={25} color={tintColor} />
-        )
-        */
-      });
-     
-      //######################
-      //ADD YOUR CODE HERE!!!!!
-      //######################
-      render() {
-        const { navigate } = this.props.navigation;
-        return (
-          <View style={styles.root}>
-            <Text style={styles.text1}>
-              Make an offer:
-            </Text>
-            <Text style={styles.text2}>
-              $99
-            </Text>
-            <TouchableOpacity style={styles.button1}>
-              <Text style={styles.text3}>
-                Message Seller...
-              </Text> 
-            </TouchableOpacity>
-            <Image
-	            source={require("../../assets/icon.png")}
-              style={styles.image1}
-              resizeMode="cover"
-            />
-            <TouchableOpacity style={styles.button1}>
-              <Text style={styles.text3}>Make Offer</Text>
-            </TouchableOpacity>
-          </View>
-        );
-      }
+  static navigationOptions = ({ navigation }) => ({
+    //tabBarVisible: false,
+    title: 'Make an Offer',
+    tabBarLabel: 'MakeOfferScreen',
+    headerTitleStyle: {
+      textAlign: 'center',
+      alignSelf: 'center'
     }
-    const styles = StyleSheet.create({
-      root: { 
-        backgroundColor: "white",
-        flex: 1,
-        alignItems: "center"
-      },
-      text1: {
+  });
 
-      },
-      text2: {
-        fontSize: 50
-      },
-      rect1: {
-        backgroundColor: "blue",
-        height: 60
-      },
-      image1: {
-        height: 250,
-        width: 400,
-        left: 0,
-        right: 0
-      },
-      text3: {
-        color: "white",
-      },
-      button1: {
-        backgroundColor: "blue",
-        height: 50,
-        width: 200,
-        left: 0,
-        right: 0,
-        opacity: 1,
-        alignItems: "center",
-        justifyContent: "center"
-      },
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Image
+        style={styles.backgroundImage}
+        source={require("../../assets/item01.png")}
+      >
+        <View style={styles.root}>
+          <View style={styles.topArea}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="$145"
+              onChangeText={(text) => this.setState({text})}
+            />
+            <Text style={styles.text}>Enter your offer for [item name]</Text>
+          </View>
+          <TouchableOpacity 
+            style={styles.offerBtn}
+            onPress={() => {console.log("Button pressed")}}
+            >
+            <Text style={styles.btnText}>Make offer</Text>
+          </TouchableOpacity>
+       </View>
+      </Image>
+    );
+  }
+}
 
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    height: null,
+    width: null,
+  },
 
+  root: { 
+    flex: 1,
+    alignItems: "center"
+  },
 
+  topArea: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    maxHeight: 100,
+    backgroundColor: "transparent"
+  },
 
+  textInput: {
+    width: "80%",
+    backgroundColor: "white",
+    textAlign: "center",
+    padding: 20,
+    marginTop: 100,
+  },
 
-    });
+  text: {
+    width: "100%",
+    color: "white",
+    textAlign: "center",
+    marginTop: 20,
+  },
+
+  offerBtn: {
+    backgroundColor: "#33a3f3",
+    width: "80%",
+    height: 100,
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: "absolute",
+    bottom: 60,
+  },
+
+  btnText: {
+
+  }
+});
